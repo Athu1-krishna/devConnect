4fdb5de6b7c95b408e1af388684c6c70c460b193
Athu1-krishna <athul.krishna.0018@gmail.com>
2025-06-24 00:00:10 +0530

    connections

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ const {connectDB} = require('./config/database')
 const cookieParser = require('cookie-parser')
 const authRouter = require('./routes/auth');
 const profileRouter = require('./routes/profile');
-const requestRouter = require('./routes/request')
+const requestRouter = require('./routes/request');
+const userRouter = require('./routes/user');
 app.use(express.json())
 app.use(cookieParser())
 
 app.use(authRouter);
 app.use(profileRouter);
 app.use(requestRouter);
+app.use(userRouter);
 
 // // feed api
 // app.get('/feed', async (req, res) => {
